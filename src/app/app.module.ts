cace8388f313d198f295bb702075bf9ff3cadc8c
Rocio Coccia <rociococcia@gmail.com>
2023-01-26 20:48:34 -0300

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuIzqComponent } from './componentes/menu-izq/menu-izq.component';
+import { ToolbarComponent } from './componentes/toolbar/toolbar.component';
+import { ContenidoComponent } from './componentes/contenido/contenido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuIzqComponent,
+    ToolbarComponent,
+    ContenidoComponent
   ],
   imports: [
     BrowserModule
